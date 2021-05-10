5127937ea0efde3e200e88e1ddc98a18c91341ae
NSerus <nunoworkingserus@gmail.com>
2021-05-10 17:54:05 +0100

    Changed schema

--- Models/model.js
+++ Models/model.js
@@ -1,13 +1,51 @@
 var mongoose = require('mongoose');  //using mongoose//the schema of the model, aka the structure
 var PlanSchema = mongoose.Schema({
-    name: {
+    basic_info:{
+        name: {
+            type: String,
+            required: true
+        },
+        date_of_birth: {
+            type: Date,
+            required: true
+        },
+        address:{
+            type: String,
+            required: true
+        },
+        emergency_phone:{
+            type: Number,
+            required: true
+        },
+        care_plan:{
+            type: String,
+            required: false
+        }
+    },
+
+    health_history: {
+        medication:{
+            type: String,
+            required: false
+        },
+
+        allergies:{
+            type: String,
+            required: false
+        },
+        mental_state:{
+            type: String,
+            required: false
+        }
+    },
+
+    Diagnosis: {
         type: String,
         required: true
     },
-    cellphone: {
-        type: String,
-        required: true
-    }
+
+
+
 });// Export Plan Model
 var Plan = module.exports = mongoose.model('plan', PlanSchema);module.exports.get = function (callback, limit) {
     Plan.find(callback).limit(limit); 
