f46e5b58a2ad1d5c20fff98d1296e881ca2d4614
NSerus <nunoworkingserus@gmail.com>
2021-05-11 15:33:14 +0100

    Modified 'add' function

--- Controller/controller.js
+++ Controller/controller.js
@@ -21,10 +21,25 @@ exports.index = (req, res)=>
 exports.add = (req, res)=>
 {
     var plan = new Plan();
-    plan.name = req.body.name? req.body.name: plan.name;
-    plan.age = req.body.age;//Saving and error checking
-    plan.health_history.health = req.body.health_history.health;
-    plan.health_history.na = req.body.health_history.na;
+    //Info basica
+    plan.basic_info.name = req.body.basic_info.name? req.body.name: plan.name;
+    plan.basic_info.date_of_birth = req.body.basic_info.date_of_birth;
+    plan.basic_info.address = req.body.basic_info.address;
+    plan.basic_info.emergency_phone = req.body.basic_info.emergency_phone;
+    plan.basic_info.care_plan = req.body.basic_info.care_plan;
+    
+    //historia médica
+    plan.health_history.medication = req.body.health_history.medication;
+    plan.health_history.allergies = req.body.health_history.allergies;
+    plan.health_history.mental_state = req.body.health_history.mental_state;
+
+    //Dados de consulta
+    plan.consult.consult_date = req.body.consult.consult_date;
+    plan.consult.exams.type_exams = req.body.consult.exams.type_exams;
+    plan.consult.exams.date_exams = req.body.consult.exams.date_exams;
+    plan.consult.medications = req.body.consult.medications;
+    plan.consult.description = req.body.consult.description;
+
     plan.save((err)=>
     {
         if (err) res.json(err);
